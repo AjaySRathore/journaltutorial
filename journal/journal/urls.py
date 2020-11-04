0dd2276511f3f29c3d158b30b9b5128ac00baf7c
digitalhoboxx <73903490+digitalhoboxx@users.noreply.github.com>
2020-11-04 14:50:12 +0200

    Fixed bug for migrate command
    Imported settings module in urls.py

--- journal/journal/urls.py
+++ journal/journal/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 Serving Static files during development
     Add static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) to
-    urlpatterns 
+    urlpatterns
 """
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
