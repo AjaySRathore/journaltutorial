03b96e0147c09ff1dfe2c2350e34b35ca5b14bc7
ajay <ajay.rathore.sr@gmail.com>
2020-11-06 15:14:14 +0200

    fixed date not appearing on gratitude view

--- journal/dailylog/views.py
+++ journal/dailylog/views.py
@@ -5,7 +5,7 @@ def gratitude_view(request, pk):
     gratitude = Gratitude.objects.get(id=pk)
     context = {
         "gratitude_value" : gratitude.gratitude,
-        "today_date" : gratitude.day_id
+        "today_date" : gratitude.day_id.day
     }
     template_name = "dailylog/gratitude.html"
     return render(request, template_name, context)
