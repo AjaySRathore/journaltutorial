f0b5939b2a0b47b6dbbdf7fe7a0e8d9731a8fa18
ajay <ajay.rathore.sr@gmail.com>
2020-11-06 15:21:25 +0200

    make day appear as a string in Day model

--- journal/dailylog/models.py
+++ journal/dailylog/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Day(models.Model):
     day = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.day)
+
 class Gratitude(models.Model):
     gratitude = models.CharField(max_length=1000)
     day_id = models.ForeignKey(Day,on_delete=models.PROTECT)
